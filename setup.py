e856b9d473d51f161716f1f4f0476f77f4d79dd4
Yannick PEROUX <yannick.peroux@gmail.com>
2014-09-08 14:07:20 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='watson',
+    version='0.1',
+    py_modules=['watson'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        watson=watson:cli
+    ''',
+)
