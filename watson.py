642e288b203b4d6dbe15c3c46ac3a76f43c5052d
Yannick PEROUX <yannick.peroux@gmail.com>
2014-09-16 16:33:39 +0200

    Add docstrings. Fix #8

--- watson.py
+++ watson.py
@@ -10,6 +10,10 @@ WATSON_FILE = os.path.join(os.path.expanduser('~'), '.watson')
 
 
 def get_watson():
+    """
+    Return the content of the current Watson file as a dict.
+    If the file doesn't exist, return an empty dict.
+    """
     try:
         with open(WATSON_FILE) as f:
             return json.load(f)
@@ -24,6 +28,9 @@ def get_watson():
 
 
 def save_watson(content):
+    """
+    Save the given dict in the Watson file. Create the file in necessary.
+    """
     try:
         with open(WATSON_FILE, 'w+') as f:
             return json.dump(content, f, indent=2)
@@ -34,6 +41,10 @@ def save_watson(content):
 
 
 def project_name(project, subproject):
+    """
+    Helper to get the right display name of a project,
+    regarding if were are in a subproject or not.
+    """
     if subproject:
         return "{}/{}".format(project, subproject)
     else:
@@ -42,6 +53,18 @@ def project_name(project, subproject):
 
 @click.group()
 def cli():
+    """
+    Watson is a tool aimed at helping you monitoring your time.
+
+    You just have to tell Watson when you start working on your
+    project with the `start` command, and you can stop the timer
+    when you're done with the `stop` command.
+
+    Projects can be divided in sub-projects by giving the projet and
+    the name of the sub-project to the `start` command.
+    """
+    # This is the main command group, needed by click in order
+    # to handle the subcommands
     pass
 
 
@@ -49,6 +72,17 @@ def cli():
 @click.argument('project')
 @click.argument('subproject', required=False)
 def start(project, subproject):
+    """
+    Start monitoring the time for the given project.
+
+    You can specify a subproject by separating the project
+    and the subproject by either a space or a `/`.
+
+    \b
+    Example :
+    $ watson start apollo11/reactor
+    Starting apollo11/reactor at 16:34
+    """
     watson = get_watson()
     start_time = arrow.now()
 
@@ -64,7 +98,7 @@ def start(project, subproject):
         )
 
     click.echo(
-        ("Starting {} at {:HH:mm:ss}"
+        ("Starting {} at {:HH:mm}"
          .format(project_name(project, subproject), start_time.to('local')))
     )
 
@@ -83,6 +117,14 @@ def start(project, subproject):
 @click.option('-m', '--message', default=None,
               help="Add a message to this frame")
 def stop(message):
+    """
+    Stop monitoring time for the current project or subproject
+
+    \b
+    Example:
+    $ watson stop
+    Stopping project apollo11/reactor, started a minute ago
+    """
     watson = get_watson()
     stop_time = arrow.now()
     current = watson.get('current')
@@ -126,6 +168,10 @@ def stop(message):
 
 @cli.command()
 def cancel():
+    """
+    Cancel the last call to the start command. The time will
+    not be recorded.
+    """
     watson = get_watson()
     current = watson.get('current')
 
@@ -138,6 +184,14 @@ def cancel():
 
 @cli.command()
 def status():
+    """
+    Display the time spent since the current project was started.
+
+    \b
+    Example:
+    $ watson status
+    Project apollo11/reactor started seconds ago
+    """
     watson = get_watson()
 
     current = watson.get('current')
