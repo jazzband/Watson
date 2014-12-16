8b4d8b059887f5ae635fb1b873617fe4d62b4cda
Yannick PEROUX <yannick.peroux@gmail.com>
2014-12-16 17:02:42 +0100

    Add help for watson log

--- README.md
+++ README.md
@@ -48,6 +48,43 @@ $ watson status
 Project apollo11/reactor started seconds ago
 ```
 
+### log
+Display a summary of the time spent on each project.
+
+If a project is given, the time spent on this project and
+each subproject is printed. Else, print the total for each root
+project.
+
+By default, the time spent the last 7 days is printed. This timespan
+can be controlled with the `--from` and `--to` arguments. The dates
+must have the format `YEAR-MONTH-DAY`, like: `2014-05-19`.
+
+```
+$ watson log
+Mon 05 May 2014 -> Mon 12 May 2014
+
+ 23h 53m 16s apollo11
+  7h 06m 08s hubble
+  1h 06m 53s voyager1
+ 12h 19m 53s voyager2
+
+Total: 44h 26m 10s
+
+$ watson log --from 2014-04-01 --to 2014-04-30  apollo11
+Tue 01 April 2014 -> Wed 30 April 2014
+
+  1h 32m 54s apollo11
+  8h 28m 09s apollo11/lander
+ 14h 15m 07s apollo11/lander/brakes
+  9h 37m 34s apollo11/lander/parachute
+ 11h 04m 39s apollo11/lander/steering
+  6h 23m 38s apollo11/lander/wheels
+  3h 28m 44s apollo11/module
+ 11h 23m 27s apollo11/reactor
+
+Total: 66h 14m 12s
+```
+
 ### projects
 
 Display the list of all the existing projects.
--- watson/cli.py
+++ watson/cli.py
@@ -164,6 +164,48 @@ def status(watson):
               "Defaults to tomorrow.")
 @click.pass_obj
 def log(watson, project, from_, to):
+    """
+    Display a summary of the time spent on each project.
+
+    If a project is given, the time spent on this project and
+    each subproject is printed. Else, print the total for each root
+    project.
+
+    By default, the time spent the last 7 days is printed. This timespan
+    can be controlled with the '--from' and '--to' arguments. The dates
+    must have the format 'YEAR-MONTH-DAY', like: '2014-05-19'.
+
+    \b
+    Example:
+    $ watson log
+    Mon 05 May 2014 -> Mon 12 May 2014
+
+    \b
+     23h 53m 16s apollo11
+      7h 06m 08s hubble
+      1h 06m 53s voyager1
+     12h 19m 53s voyager2
+
+    \b
+    Total: 44h 26m 10s
+
+    \b
+    $ watson log --from 2014-04-01 --to 2014-04-30  apollo11
+    Tue 01 April 2014 -> Wed 30 April 2014
+
+    \b
+      1h 32m 54s apollo11
+      8h 28m 09s apollo11/lander
+     14h 15m 07s apollo11/lander/brakes
+      9h 37m 34s apollo11/lander/parachute
+     11h 04m 39s apollo11/lander/steering
+      6h 23m 38s apollo11/lander/wheels
+      3h 28m 44s apollo11/module
+     11h 23m 27s apollo11/reactor
+
+    \b
+    Total: 66h 14m 12s
+    """
     if project:
         projects = (p for p in watson.projects
                     if p == project or p.startswith(project + '/'))
