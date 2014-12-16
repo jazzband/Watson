3e99c40a90947bc0bc67b7075a0963fda75e7d36
Yannick PEROUX <yannick.peroux@gmail.com>
2014-12-16 16:34:59 +0100

    Improve utils.format_timedelta

--- watson/utils.py
+++ watson/utils.py
@@ -5,19 +5,19 @@ def format_timedelta(delta):
     seconds = int(delta.total_seconds())
     neg = seconds < 0
     seconds = abs(seconds)
+    total = seconds
     stems = []
 
-    if seconds > 3600:
+    if total >= 3600:
         hours = seconds // 3600
         stems.append('{:2}h'.format(hours))
         seconds -= hours * 3600
 
-    if seconds > 60:
+    if total >= 60:
         mins = seconds // 60
         stems.append('{:02}m'.format(mins))
         seconds -= mins * 60
 
-    if seconds >= 0:
-        stems.append('{:02}s'.format(seconds))
+    stems.append('{:02}s'.format(seconds))
 
-    return '{:>12}'.format(('-' if neg else '') + ' '.join(stems))
+    return ('-' if neg else '') + ' '.join(stems)
