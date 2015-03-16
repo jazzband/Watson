10930c2bb8cdbdbce8b6b61bd8aa1260bb1a5b23
Yannick PEROUX <yannick.peroux@gmail.com>
2015-03-16 10:27:29 +0100

    Remove the GUI (will be moved in another package)

--- setup.py
+++ setup.py
@@ -3,21 +3,15 @@ from setuptools import setup
 setup(
     name='watson',
     version='0.1',
-    packages=['watson', 'gui'],
+    packages=['watson'],
     install_requires=[
         'Click',
         'arrow',
         'requests',
     ],
-    extras_require={
-        'gui': ['PySide']
-    },
     entry_points={
         'console_scripts': [
             'watson = watson.__main__:cli',
-        ],
-        'gui_scripts': [
-            'watson-systray = gui.__main__:main',
         ]
     }
 )
