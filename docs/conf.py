71fef2b3076f3d1649f17b6d8a37658d911e3ff9
Julien Maupetit <julien@tailordev.fr>
2016-01-21 18:17:06 +0100

    Use google style docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ import os
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
