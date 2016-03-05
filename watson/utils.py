8567649a0e7166859b5f4e5e891d4ce4c1f1e622
Scott Hansen <firecat4153@gmail.com>
2016-03-04 19:10:44 -0800

    Leverage arrow timedelta property to simplify format_timedelta()

--- watson/utils.py
+++ watson/utils.py
@@ -41,25 +41,9 @@ def format_timedelta(delta):
     """
     Return a string roughly representing a timedelta.
     """
-    seconds = int(delta.total_seconds())
-    neg = seconds < 0
-    seconds = abs(seconds)
-    total = seconds
-    stems = []
-
-    if total >= 3600:
-        hours = seconds // 3600
-        stems.append('{}h'.format(hours))
-        seconds -= hours * 3600
-
-    if total >= 60:
-        mins = seconds // 60
-        stems.append('{:02}m'.format(mins))
-        seconds -= mins * 60
-
-    stems.append('{:02}s'.format(seconds))
-
-    return ('-' if neg else '') + ' '.join(stems)
+    neg = '-' if int(delta.total_seconds()) < 0 else ''
+    res = "{}h {}m {}s".format(*str(abs(delta)).split(":"))
+    return "{}{}".format(neg, res)
 
 
 def sorted_groupby(iterator, key, reverse=False):
