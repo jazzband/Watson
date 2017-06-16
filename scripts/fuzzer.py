b8970ee6111cef81d4791d06e1087b25c4987957
Christopher Arndt <chris@chrisarndt.de>
2017-06-16 16:06:39 +0200

    Add shebang line to maintainer scripts
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- scripts/fuzzer.py
+++ scripts/fuzzer.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import arrow
 import random
 import os
--- scripts/gen-cli-docs.py
+++ scripts/gen-cli-docs.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import inspect
 
 from click.core import Command, Context
