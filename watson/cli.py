31970c9584ff17ebc74211ea8cf27eeebb251006
Antti Peltonen <antti.peltonen@iki.fi>
2018-03-21 10:38:51 +0200

    Date option naming and handling bugs

--- watson/cli.py
+++ watson/cli.py
@@ -774,7 +774,7 @@ def frames(watson):
 
 @cli.command(context_settings={'ignore_unknown_options': True})
 @click.argument('args', nargs=-1)
-@click.option('-f','--from', required=True, type=Date, help="Date and time of start of tracked activity")
+@click.option('-f','--from', 'from_', required=True, type=Date, help="Date and time of start of tracked activity")
 @click.option('-t','--to', required=True, type=Date, help="Date and time of end of tracked activity")
 @click.pass_obj
 def add(watson, args, from_, to):
@@ -802,7 +802,7 @@ def add(watson, args, from_, to):
     ))))  # pile of pancakes !
 
     # add a new frame, call watson save to update state files
-    frame = watson.add(project=project, tags=tags, from_date=from_, to_date=to_date)
+    frame = watson.add(project=project, tags=tags, from_date=from_, to_date=to)
     click.echo("Adding project {}{}, started {} and stopped {}. (id: {})".format(
         style('project', frame.project),
         (" " if frame.tags else "") + style('tags', frame.tags),
