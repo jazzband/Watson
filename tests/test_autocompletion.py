ee210c604ce05f8a26377316a80b2041627a5d3d
Max Görner <max@familie-goerner.eu>
2019-09-09 16:10:50 +0200

    Add serious tests for get_frames()
    Tests for get_frames() are added. Different to the previous ones, the
    actually test the behaviour of get_frames for three plausible use cases.

--- tests/test_autocompletion.py
+++ tests/test_autocompletion.py
@@ -1,5 +1,6 @@
 """Unit tests for the 'autocompletion' module."""
 
+import json
 import os
 import pytest
 import shutil
@@ -23,8 +24,35 @@ def prepare_sysenv_for_testing(config_dirname, monkeypatch):
 
 
 @AUTOCOMPLETION_FRAMES
-def test_get_frames(datafiles, monkeypatch):
+def test_get_frames_returns_distinct(datafiles, monkeypatch):
     prepare_sysenv_for_testing(datafiles, monkeypatch)
+    prefix = ''
+    frame_id_list = list(get_frames(None, None, prefix))
+    assert sorted(set(frame_id_list)) == sorted(frame_id_list)
 
-    all_frames = set(get_frames(None, None, ''))
-    assert len(all_frames) == 10
+
+@AUTOCOMPLETION_FRAMES
+def test_complete_empty_frame_prefix(datafiles, monkeypatch):
+    prepare_sysenv_for_testing(datafiles, monkeypatch)
+    prefix = ''
+    with open(str(AUTOCOMPLETION_FRAMES_PATH)) as fh:
+        n_frames = len(json.load(fh))
+    frame_ids = set(get_frames(None, None, prefix))
+    assert len(frame_ids) == n_frames
+
+
+@AUTOCOMPLETION_FRAMES
+def test_not_existing_frame_prefix(datafiles, monkeypatch):
+    prepare_sysenv_for_testing(datafiles, monkeypatch)
+    prefix = 'NOT-EXISTING-PREFIX'
+    frame_ids = set(get_frames(None, None, prefix))
+    assert frame_ids == set()
+
+
+@AUTOCOMPLETION_FRAMES
+def test_existing_frame_prefix(datafiles, monkeypatch):
+    prepare_sysenv_for_testing(datafiles, monkeypatch)
+    prefix = 'f4f7'
+    frame_ids = set(get_frames(None, None, prefix))
+    assert len(frame_ids) == 2
+    assert all(cur_frame_id.startswith(prefix) for cur_frame_id in frame_ids)
