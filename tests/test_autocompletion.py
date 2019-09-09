45e578e4b1398b84bb4ea9e5392aa4f812049791
Max Görner <max@familie-goerner.eu>
2019-09-09 11:40:46 +0200

    CleanUp Get rid of old watson fixture
    The old watson fixture was deleted. The tested functions start their own
    Watson instance, relying on the environment variable WATSON_DIR.
    Therefore, the fixture is not needed anymore.

--- tests/test_autocompletion.py
+++ tests/test_autocompletion.py
@@ -1,11 +1,9 @@
 """Unit tests for the 'autocompletion' module."""
 
-import json
 import os
 import pytest
 import shutil
 
-from watson import Watson
 from watson.autocompletion import (
     get_frames,
 )
@@ -22,25 +20,6 @@ def prepare_sysenv_for_testing(config_dirname, monkeypatch):
         os.path.join(str(config_dirname), "frames"),
         )
     monkeypatch.setenv('WATSON_DIR', str(config_dirname))
-
-
-@pytest.fixture
-def frames_file():
-    return str(AUTOCOMPLETION_FRAMES_PATH)
-
-
-@pytest.fixture
-def watson(config_dir, frames_file):
-    in_frames_file = frames_file
-    with open(in_frames_file) as in_frames_fh:
-        list_of_frames = json.load(in_frames_fh)
-    return Watson(config_dir=config_dir, frames=list_of_frames)
-
-
-@AUTOCOMPLETION_FRAMES
-def test_watson(watson):
-    projects = set(cur_frame[2] for cur_frame in watson.frames.dump())
-    assert projects == set(('foo', 'bar'))
 
 
 @AUTOCOMPLETION_FRAMES
