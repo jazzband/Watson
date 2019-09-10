630bafdf93736c5317523a36d568d897a4be7d82
Max Görner <max@familie-goerner.eu>
2019-09-10 12:07:20 +0200

    StyleFix Adapt code formatting using black
    The format of the code was a bit messed up. It was once reformatted
    using black in order to improve the readability a bit.

--- tests/test_autocompletion.py
+++ tests/test_autocompletion.py
@@ -2,8 +2,9 @@
 
 import json
 import os
-import pytest
 import shutil
+
+import pytest
 
 from watson.autocompletion import (
     get_frames,
@@ -16,13 +17,13 @@ from watson.autocompletion import (
 from .conftest import TEST_FIXTURE_DIR
 
 
-AUTOCOMPLETION_FRAMES_PATH = TEST_FIXTURE_DIR / 'frames-for-autocompletion'
+AUTOCOMPLETION_FRAMES_PATH = TEST_FIXTURE_DIR / "frames-for-autocompletion"
 AUTOCOMPLETION_FRAMES = pytest.mark.datafiles(AUTOCOMPLETION_FRAMES_PATH)
 with open(str(AUTOCOMPLETION_FRAMES_PATH)) as fh:
     N_FRAMES = len(json.load(fh))
 
 
-class CTXDummy():
+class CTXDummy:
     def __init__(self, rename_type):
         self.params = {"rename_type": rename_type}
 
@@ -31,94 +32,91 @@ def prepare_sysenv_for_testing(config_dirname, monkeypatch):
     shutil.copy(
         os.path.join(str(config_dirname), "frames-for-autocompletion"),
         os.path.join(str(config_dirname), "frames"),
-        )
-    monkeypatch.setenv('WATSON_DIR', str(config_dirname))
+    )
+    monkeypatch.setenv("WATSON_DIR", str(config_dirname))
 
 
 @AUTOCOMPLETION_FRAMES
-@pytest.mark.parametrize('func_to_test, ctx', [
-    (get_frames, None),
-    (get_project_or_task_completion, None),
-    (get_projects, None),
-    (get_rename_name, CTXDummy("project")),
-    (get_rename_name, CTXDummy("tag")),
-    (get_rename_types, None),
-    (get_tags, None),
-])
+@pytest.mark.parametrize(
+    "func_to_test, ctx",
+    [
+        (get_frames, None),
+        (get_project_or_task_completion, None),
+        (get_projects, None),
+        (get_rename_name, CTXDummy("project")),
+        (get_rename_name, CTXDummy("tag")),
+        (get_rename_types, None),
+        (get_tags, None),
+    ],
+)
 def test_if_returned_values_are_distinct(
-    datafiles,
-    monkeypatch,
-    func_to_test,
-    ctx,
+    datafiles, monkeypatch, func_to_test, ctx
 ):
     prepare_sysenv_for_testing(datafiles, monkeypatch)
-    prefix = ''
+    prefix = ""
     ret_list = list(func_to_test(ctx, [], prefix))
     assert sorted(ret_list) == sorted(set(ret_list))
 
 
 @AUTOCOMPLETION_FRAMES
-@pytest.mark.parametrize('func_to_test, n_expected_returns, ctx', [
-    (get_frames, N_FRAMES, None),
-    (get_project_or_task_completion, 5, None),
-    (get_projects, 5, None),
-    (get_rename_name, 5, CTXDummy("project")),
-    (get_rename_name, 3, CTXDummy("tag")),
-    (get_rename_types, 2, None),
-    (get_tags, 3, None),
-])
+@pytest.mark.parametrize(
+    "func_to_test, n_expected_returns, ctx",
+    [
+        (get_frames, N_FRAMES, None),
+        (get_project_or_task_completion, 5, None),
+        (get_projects, 5, None),
+        (get_rename_name, 5, CTXDummy("project")),
+        (get_rename_name, 3, CTXDummy("tag")),
+        (get_rename_types, 2, None),
+        (get_tags, 3, None),
+    ],
+)
 def test_if_empty_prefix_returns_everything(
-    datafiles,
-    monkeypatch,
-    func_to_test,
-    n_expected_returns,
-    ctx,
+    datafiles, monkeypatch, func_to_test, n_expected_returns, ctx
 ):
     prepare_sysenv_for_testing(datafiles, monkeypatch)
-    prefix = ''
+    prefix = ""
     completed_vals = set(func_to_test(ctx, [], prefix))
     assert len(completed_vals) == n_expected_returns
 
 
 @AUTOCOMPLETION_FRAMES
-@pytest.mark.parametrize('func_to_test, ctx', [
-    (get_frames, None),
-    (get_project_or_task_completion, None),
-    (get_projects, None),
-    (get_rename_name, CTXDummy("project")),
-    (get_rename_name, CTXDummy("tag")),
-    (get_rename_types, None),
-    (get_tags, None),
-])
+@pytest.mark.parametrize(
+    "func_to_test, ctx",
+    [
+        (get_frames, None),
+        (get_project_or_task_completion, None),
+        (get_projects, None),
+        (get_rename_name, CTXDummy("project")),
+        (get_rename_name, CTXDummy("tag")),
+        (get_rename_types, None),
+        (get_tags, None),
+    ],
+)
 def test_completion_of_nonexisting_prefix(
-    datafiles,
-    monkeypatch,
-    func_to_test,
-    ctx,
+    datafiles, monkeypatch, func_to_test, ctx
 ):
     prepare_sysenv_for_testing(datafiles, monkeypatch)
-    prefix = 'NOT-EXISTING-PREFIX'
+    prefix = "NOT-EXISTING-PREFIX"
     ret_list = list(func_to_test(ctx, [], prefix))
     assert not ret_list
 
 
 @AUTOCOMPLETION_FRAMES
-@pytest.mark.parametrize('func_to_test, prefix, n_expected_vals, ctx', [
-    (get_frames, 'f4f7', 2, None),
-    (get_project_or_task_completion, 'project3', 2, None),
-    (get_projects, 'project3', 2, None),
-    (get_rename_name, 'project3', 2, CTXDummy("project")),
-    (get_rename_name, 'tag', 3, CTXDummy("tag")),
-    (get_rename_types, 'ta', 1, None),
-    (get_tags, 'tag', 3, None),
-])
+@pytest.mark.parametrize(
+    "func_to_test, prefix, n_expected_vals, ctx",
+    [
+        (get_frames, "f4f7", 2, None),
+        (get_project_or_task_completion, "project3", 2, None),
+        (get_projects, "project3", 2, None),
+        (get_rename_name, "project3", 2, CTXDummy("project")),
+        (get_rename_name, "tag", 3, CTXDummy("tag")),
+        (get_rename_types, "ta", 1, None),
+        (get_tags, "tag", 3, None),
+    ],
+)
 def test_completion_of_existing_prefix(
-    datafiles,
-    monkeypatch,
-    func_to_test,
-    prefix,
-    n_expected_vals,
-    ctx,
+    datafiles, monkeypatch, func_to_test, prefix, n_expected_vals, ctx
 ):
     prepare_sysenv_for_testing(datafiles, monkeypatch)
     ret_set = set(func_to_test(ctx, [], prefix))
@@ -127,17 +125,16 @@ def test_completion_of_existing_prefix(
 
 
 @AUTOCOMPLETION_FRAMES
-@pytest.mark.parametrize('func_to_test, prefix, expected_vals', [
-    (get_rename_types, "", ["project", "tag"]),
-    (get_rename_types, "t", ["tag"]),
-    (get_rename_types, "p", ["project"]),
-])
+@pytest.mark.parametrize(
+    "func_to_test, prefix, expected_vals",
+    [
+        (get_rename_types, "", ["project", "tag"]),
+        (get_rename_types, "t", ["tag"]),
+        (get_rename_types, "p", ["project"]),
+    ],
+)
 def test_for_known_completion_values(
-    datafiles,
-    monkeypatch,
-    func_to_test,
-    prefix,
-    expected_vals
+    datafiles, monkeypatch, func_to_test, prefix, expected_vals
 ):
     prepare_sysenv_for_testing(datafiles, monkeypatch)
     ret_list = list(func_to_test(None, [], prefix))
