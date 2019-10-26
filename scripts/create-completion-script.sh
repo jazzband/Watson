50251af4ef40f43e21cf24c7459bf907d12ebd22
Max Görner <max@familie-goerner.eu>
2019-10-26 22:18:47 +0200

    Stop deploying completion receipts.
    In an effort to adapt the code to the existing documentation, the script
    is rewritten such that it does not deploy the generated receipts.
    Instead, it will write them to a file.

--- scripts/create-completion-script.sh
+++ scripts/create-completion-script.sh
@@ -7,10 +7,10 @@ function print_help() {
   cat <<EOF
 Usage: $0 shell-type
 
-This script generates the auto completion receipt required by Bash or Zsh and
-automatically puts it into the systems folder. Since the receipt is only a
-wrapper around the click framework, this results in correct tab completion,
-regardless of the currently used version watson.
+This script generates the auto completion receipt required by Bash or Zsh.
+Since the generated receipt is only a wrapper around the click framework, this
+results in correct tab completion, regardless of the currently used version
+watson.
 
 The argument shell-type must be either "bash" or "zsh".
 EOF
@@ -36,18 +36,15 @@ case $1 in
     ;;
   bash)
     src_command="source"
+    target_file="watson.completion"
     ;;
   zsh)
     src_command="source_zsh"
+    target_file="watson.zsh-completion"
     ;;
   *)
     echo "Unknown argument '$1'. Please consult help text." >&2
     exit 1
 esac
 
-
-# NOTE: Putting `sudo` in the very beginning might result in executing the
-# wrong `watson` command or in a failure altogether. `sudo` changes the user
-# to root and the root user might not be aware of the currently active virtual
-# environment etc.
-_WATSON_COMPLETE=$src_command watson | sudo tee /etc/bash_completion.d/watson >/dev/null
+_WATSON_COMPLETE=$src_command watson > "$target_file"
